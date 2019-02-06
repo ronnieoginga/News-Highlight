49d94e2c4ef104d1535a23949682f3e83aa187e9
ronnieoginga <oginga001ronney@gmail.com>
2019-02-06 09:51:35 +0300

    souces test removed error

--- tests/sources_test.py
+++ tests/sources_test.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import Sources
+from models import Sources
 
 Sources = sources.Sources
 
@@ -8,13 +8,14 @@ class sources_test(unittest.TestCase):
     '''
     this function creates an object of the Sources class before each and every tesy
     '''
+
     def setUp(self):
         self.new_source = Sources(
-            'ABC-news', 'ABC-news','dk', 'https://abc.com', 'us')
+            'ABC-news', 'ABC-news', 'dk', 'https://abc.com', 'us')
 
     def test_instance(self):
         '''
         this is a function to assert whether the instance is really an instance of our class
         '''
         self.assertTrue(isinstance(self.new_source, Sources))
-
+        

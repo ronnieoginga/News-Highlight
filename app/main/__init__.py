26322d36096a1169331c7f23d066fef47262ea02
ronnieoginga <oginga001ronney@gmail.com>
2019-02-06 13:13:41 +0300

    updated init

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,8 +3,8 @@ main = Blueprint('main',__name__)
 from . import views,error
 
 def create_app(config_name):
-   
     app = Flask(__name__)
+
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
--- config.py
+++ config.py
@@ -3,7 +3,6 @@ class Config:
     '''
     General configuration parent class
     '''
-
     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     SOURCES_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
